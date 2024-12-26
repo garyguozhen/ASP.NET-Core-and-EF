0c96cc59d9cf821c7c57871805bd164073bef6c6
garyguo <garyguo19860502@outlook.com>
2024-12-26 15:14:02 +0800

    Add db project

---
+++ Chapter4_LeaveManagmentSystemDB.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chapter4_LeaveManagmentSystemDB.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
